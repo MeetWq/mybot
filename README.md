b3f4552eadc1b5059388c096022266e57253e64b
wangqian <meetwq@gmail.com>
2021-01-23 18:03:50 +0800

    restart

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 #### 简介
 
-基于 [Nonebot](https://github.com/nonebot/nonebot) 和 [酷Q](https://cqp.cc/) 的QQ机器人
-
-主要功能是 cc98 和 nhd 自动签到信息的发送
-以及基于 [cc98api](https://github.com/zjuchenyuan/cc98api) 实现的 cc98快速看帖功能
-
-机器人QQ号为 `1846731675` ，欢迎骚扰
-
-
-#### 安装
-安装 miniconda
-```bash
-conda install requests python-Levenshtein ujson
-pip install nonebot
-pip install "nonebot[scheduler]"
-pip install fuzzywuzzy aiocache msgpack
-pip install -U cos-python-sdk-v5
-pip install aiohttp
-```
+基于 [Mirai](https://github.com/mamoe/mirai) 和 [Nonebot](https://github.com/nonebot/nonebot2) 实现的QQ机器人
--- bot.py
+++ bot.py
@@ -1,18 +1,34 @@
-from os import path
-import nonebot
-import config
-import logging
-from nonebot.log import logger
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-if __name__ == '__main__':
-    nonebot.init(config)
-    nonebot.load_builtin_plugins()
-    nonebot.load_plugins(path.join(path.dirname(__file__), 'my', 'plugins'), 'my.plugins')
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(path.join('my', 'log', 'nonebot.log'))
-    file_handler.setFormatter(logging.Formatter(
-        '[%(asctime)s %(name)s] %(levelname)s: %(message)s'
-    ))
-    file_handler.setLevel(logging.DEBUG)
-    logger.addHandler(file_handler)
-    nonebot.run()
+import nonebot
+from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+
+# Custom your logger
+# 
+from nonebot.log import logger, default_format
+
+logger.add("error.log",
+           rotation="00:00",
+           diagnose=False,
+           level="ERROR",
+           format=default_format)
+
+# You can pass some keyword args config to init function
+nonebot.init()
+app = nonebot.get_asgi()
+
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", CQHTTPBot)
+
+nonebot.load_builtin_plugins()
+nonebot.load_plugins("src/plugins")
+
+# Modify some config / config depends on loaded configs
+# 
+# config = driver.config
+# do something...
+
+
+if __name__ == "__main__":
+    nonebot.run(app="bot:app")
