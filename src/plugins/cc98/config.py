e492bca20ed312c98b8aa0deb6b90f840c2291bb
wangqian <meetwq@gmail.com>
2021-03-30 15:12:15 +0800

    remove cc98, cherria

--- src/plugins/cc98/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Config(BaseSettings):
-    cc98_user_name: str = ''
-    cc98_user_password: str = ''
-
-    class Config:
-        extra = "ignore"
--- src/plugins/cherria/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Config(BaseSettings):
-    cherria_group: list = []
-
-    class Config:
-        extra = "ignore"
