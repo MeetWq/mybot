35c625c9c254dae5ee6e5df37009c1ef537500ba
wangqian <meetwq@gmail.com>
2021-04-02 11:38:47 +0800

    add make emoji

--- src/plugins/emoji/__init__.py
+++ src/plugins/emoji/__init__.py
@@ -1,14 +1,18 @@
 import os
-from nonebot import export, on_regex, on_endswith
+from nonebot import export, on_regex, on_endswith, on_shell_command
+from nonebot.rule import ArgumentParser
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Bot, Event, MessageSegment
 
-from .data_source import get_emoji_path, get_image
+from nonebot.log import logger
+
+from .data_source import get_emoji_path, get_image, make_emoji, emojis
 
 export = export()
 export.description = '表情包'
-export.usage = 'Usage:\n  1. 98表情包，包含"cc98xx"、"acxx"、"tbxx"、"emxx"、"msxx"、"a/c/f:xxx"\n' \
-    '  2. {keyword}.jpg，将会回复相关的表情包'
+export.usage = 'Usage:\n  1、98表情包，包含"cc98xx"、"acxx"、"tbxx"、"emxx"、"msxx"、"a/c/f:xxx"\n' \
+    '  2、{keyword}.jpg，将会回复相关的表情包\n' \
+    '  3、emoji -t {type} {text}，表情包制作，发送 "emoji list" 查看支持的表情包列表'
 export.notice = 'Notice:\n  由于NHD表情"emxx"与98冲突，用"emmxx"代替'
 export.help = export.description + '\n' + export.usage + '\n' + export.notice
 
@@ -48,3 +52,48 @@ async def _(bot: Bot, event: Event, state: T_State):
         await get_jpg.finish(message="找不到相关的图片")
     await get_jpg.send(message=MessageSegment.image(file=img_url))
     await get_jpg.finish()
+
+
+emoji_parser = ArgumentParser()
+emoji_parser.add_argument('-t', '--type', type=str)
+emoji_parser.add_argument('text', nargs='+')
+
+emoji = on_shell_command('emoji', aliases={'表情包'}, parser=emoji_parser, priority=28)
+
+
+@emoji.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    help_msg = 'Usage:\n  emoji -t {type} {text}，发送 "emoji list" 查看支持的表情包列表'
+
+    args = state['args']
+    if not hasattr(args, 'text'):
+        await emoji.finish(help_msg)
+
+    if args.text[0] == 'list':
+        message = '支持的表情包：'
+        for i, e in enumerate(emojis):
+            message += f"\n{i}. {'/'.join(e['names'])}，需要 {e['input_num']} 个输入"
+        await emoji.finish(message)
+
+    if not args.type:
+        await emoji.finish(help_msg)
+
+    type = str(args.type)
+    num = -1
+    if type.isdigit() and 0 <= int(type) < len(emojis):
+        num = int(type)
+    else:
+        for i, e in enumerate(emojis):
+            if type in e['names']:
+                num = i
+    if num == -1:
+        await emoji.finish('暂不支持制作该表情包，发送 "emoji list" 查看支持的列表')
+
+    texts = args.text
+    if len(texts) != emojis[num]['input_num']:
+        await emoji.finish(f"该表情包需要 {emojis[num]['input_num']} 个输入")
+
+    await emoji.send(message='请稍候...')
+    msg = await make_emoji(num, texts)
+    await emoji.send(message=msg)
+    await emoji.finish()
--- src/plugins/emoji/data_source.py
+++ src/plugins/emoji/data_source.py
@@ -1,14 +1,20 @@
 import re
+import uuid
 import random
 import aiohttp
 import traceback
 from pathlib import Path
 from bs4 import BeautifulSoup
 from urllib.parse import quote
+from PIL import Image, ImageFont, ImageDraw
+from nonebot.adapters.cqhttp import MessageSegment
 
 from nonebot.log import logger
 
 image_path = Path('src/data/images')
+cache_path = Path('cache/emoji').absolute()
+if not cache_path.exists():
+    cache_path.mkdir(parents=True)
 
 
 def get_emoji_path(name: str):
@@ -49,3 +55,142 @@ async def get_image(keyword):
     if image_num >= 3:
         images = images[:3]
     return random.choice(images).img['data-original']
+
+
+def text_position(image, text, font, padding_y=5):
+    text_width, text_height = font.getsize(text)
+    img_width, img_height = image.size
+    x = int((img_width - text_width) / 2)
+    y = img_height - text_height - padding_y
+    return x, y
+
+
+async def darw_text(image, x, y, text, font, shadowcolor, fillcolor):
+    draw = ImageDraw.Draw(image)
+    # thin border
+    draw.text((x - 1, y), text, font=font, fill=shadowcolor)
+    draw.text((x + 1, y), text, font=font, fill=shadowcolor)
+    draw.text((x, y - 1), text, font=font, fill=shadowcolor)
+    draw.text((x, y + 1), text, font=font, fill=shadowcolor)
+    # thicker border
+    draw.text((x - 1, y - 1), text, font=font, fill=shadowcolor)
+    draw.text((x + 1, y - 1), text, font=font, fill=shadowcolor)
+    draw.text((x - 1, y + 1), text, font=font, fill=shadowcolor)
+    draw.text((x + 1, y + 1), text, font=font, fill=shadowcolor)
+    # now draw the text over it
+    draw.text((x, y), text, font=font, fill=fillcolor)
+
+
+async def make_wangjingze(texts):
+    font_path = Path('src/data/fonts/msyh.ttc')
+    font = ImageFont.truetype(str(font_path), 20, encoding='utf-8')
+    shadowcolor = (0, 0, 0)
+    fillcolor = (255, 255, 255)
+
+    files = [Path('src/data/emojis/wangjingze/%d.jpg' % i) for i in range(0, 52)]
+    frames = [Image.open(f) for f in files]
+    parts = [frames[0:9], frames[12:24], frames[25:35], frames[37:48]]
+    for part, text in zip(parts, texts):
+        for frame in part:
+            x, y = text_position(frame, text, font)
+            if x < 5:
+                return f'“{text}”长度过长，请适当缩减'
+            await darw_text(frame, x, y, text, font, shadowcolor, fillcolor)
+    save_path = cache_path / (uuid.uuid1().hex + '.gif')
+    frames[0].save(save_path, save_all=True, append_images=frames[1:], duration=130)
+    return MessageSegment.image(file=f'file://{save_path}')
+
+
+async def make_weisuoyuwei(texts):
+    font_path = Path('src/data/fonts/msyh.ttc')
+    font = ImageFont.truetype(str(font_path), 15, encoding='utf-8')
+    shadowcolor = (0, 0, 0)
+    fillcolor = (255, 255, 255)
+
+    files = [Path('src/data/emojis/weisuoyuwei/%d.jpg' % i) for i in range(0, 125)]
+    frames = [Image.open(f) for f in files]
+    parts = [frames[8:10], frames[20:27], frames[32:45], frames[46:60],
+             frames[61:70], frames[72:79], frames[83:98], frames[109:118], frames[118:125]]
+    for part, text in zip(parts, texts):
+        for frame in part:
+            x, y = text_position(frame, text, font)
+            if x < 5:
+                return f'“{text}”长度过长，请适当缩减'
+            await darw_text(frame, x, y, text, font, shadowcolor, fillcolor)
+    save_path = cache_path / (uuid.uuid1().hex + '.gif')
+    frames[0].save(save_path, save_all=True, append_images=frames[1:], duration=170)
+    return MessageSegment.image(file=f'file://{save_path}')
+
+
+async def make_ninajiaoxihuanma(texts):
+    font_path = Path('src/data/fonts/msyh.ttc')
+    font = ImageFont.truetype(str(font_path), 20, encoding='utf-8')
+    shadowcolor = (0, 0, 0)
+    fillcolor = (255, 255, 255)
+
+    files = [Path('src/data/emojis/ninajiaoxihuanma/%d.jpg' % i) for i in range(0, 58)]
+    frames = [Image.open(f) for f in files]
+    parts = [frames[5:22], frames[26:38], frames[39:50]]
+    for part, text in zip(parts, texts):
+        for frame in part:
+            x, y = text_position(frame, text, font)
+            if x < 5:
+                return f'“{text}”长度过长，请适当缩减'
+            await darw_text(frame, x, y, text, font, shadowcolor, fillcolor)
+    save_path = cache_path / (uuid.uuid1().hex + '.gif')
+    frames[0].save(save_path, save_all=True, append_images=frames[1:], duration=100)
+    return MessageSegment.image(file=f'file://{save_path}')
+
+
+async def make_qiegewala(texts):
+    font_path = Path('src/data/fonts/msyh.ttc')
+    font = ImageFont.truetype(str(font_path), 20, encoding='utf-8')
+    shadowcolor = (0, 0, 0)
+    fillcolor = (255, 255, 255)
+
+    files = [Path('src/data/emojis/qiegewala/%d.jpg' % i) for i in range(0, 87)]
+    frames = [Image.open(f) for f in files]
+    parts = [frames[0:15], frames[16:31], frames[31:38], 
+             frames[38:48], frames[49:68], frames[68:86]]
+    for part, text in zip(parts, texts):
+        for frame in part:
+            x, y = text_position(frame, text, font)
+            if x < 5:
+                return f'“{text}”长度过长，请适当缩减'
+            await darw_text(frame, x, y, text, font, shadowcolor, fillcolor)
+    save_path = cache_path / (uuid.uuid1().hex + '.gif')
+    frames[0].save(save_path, save_all=True, append_images=frames[1:], duration=130)
+    return MessageSegment.image(file=f'file://{save_path}')
+
+
+emojis = [
+    {
+        'names': ['王境泽', '真香'],
+        'input_num': 4,
+        'func': make_wangjingze,
+    },
+    {
+        'names': ['为所欲为'],
+        'input_num': 9,
+        'func': make_weisuoyuwei,
+    },
+    {
+        'names': ['你那叫喜欢吗'],
+        'input_num': 3,
+        'func': make_ninajiaoxihuanma,
+    },
+    {
+        'names': ['切格瓦拉', '打工是不可能打工的'],
+        'input_num': 6,
+        'func': make_qiegewala,
+    }
+]
+
+
+async def make_emoji(num, texts):
+    try:
+        msg = await emojis[num]['func'](texts)
+        return msg
+    except:
+        logger.debug(traceback.format_exc())
+        return '出错了，请稍后再试'
