bb44a3cdfaa5b8b04e703925b92f2ab7bc640c85
meetwq <meetwq@gmail.com>
2021-05-05 00:24:54 +0800

    add avatar throw, crawl

--- src/plugins/avatar/__init__.py
+++ src/plugins/avatar/__init__.py
@@ -8,15 +8,19 @@ from .data_source import get_image
 
 export = export()
 export.description = '头像相关表情生成'
-export.usage = 'Usage:\n  摸/撕 {qq/@user}'
+export.usage = 'Usage:\n  摸/撕/丢/爬 {qq/@user/自己}'
 export.help = export.description + '\n' + export.usage
 
 petpet = on_startswith('摸', priority=26)
 tear = on_startswith('撕', priority=26)
+throw = on_startswith('丢', priority=26)
+crawl = on_startswith('爬', priority=26)
 
 
 @petpet.handle()
 @tear.handle()
+@throw.handle()
+@crawl.handle()
 async def _(bot: Bot, event: Event, state: T_State):
     msg = event.get_message()
     msg_text = event.get_plaintext().strip()
@@ -27,13 +31,15 @@ async def _(bot: Bot, event: Event, state: T_State):
             qq = msg_seg.data['qq']
             break
     if not qq:
-        msg_content = re.sub(r'[摸撕]', '', msg_text).strip()
+        msg_content = re.sub(r'[摸撕丢爬]', '', msg_text).strip()
         if msg_content.isdigit():
             qq = msg_content
+        elif msg_content == '自己':
+            qq = event.user_id
     logger.debug(qq)
     if qq:
         command = msg_text[0]
-        types = {'摸': 'petpet', '撕': 'tear'}
+        types = {'摸': 'petpet', '撕': 'tear', '丢': 'throw', '爬': 'crawl'}
         if command in types:
             type = types[command]
             logger.debug(type)
--- src/plugins/avatar/data_source.py
+++ src/plugins/avatar/data_source.py
@@ -1,10 +1,11 @@
+import random
 import aiohttp
 import hashlib
 import imageio
 import traceback
 from pathlib import Path
-from PIL import Image as IMG
 from nonebot.log import logger
+from PIL import Image, ImageDraw, ImageFilter
 
 data_path = Path('src/data/emojis')
 cache_path = Path('cache/avatar')
@@ -30,15 +31,15 @@ async def get_avatar(user_id, avatar_path):
 
 
 async def create_petpet(input_path, output_path):
-    avatar = IMG.open(input_path)
+    avatar = Image.open(input_path).convert('RGBA')
     hand_frames = [data_path / f'petpet/frame{i}.png' for i in range(5)]
-    hand_frames = [IMG.open(i) for i in hand_frames]
+    hand_frames = [Image.open(i) for i in hand_frames]
     frame_locs = [(14, 20, 98, 98), (12, 33, 101, 85), (8, 40, 110, 76), (10, 33, 102, 84), (12, 20, 98, 98)]
     frames = []
     for i in range(5):
-        frame = IMG.new('RGBA', (112, 112), (255, 255, 255, 0))
+        frame = Image.new('RGBA', (112, 112), (255, 255, 255, 0))
         x, y, l, w = frame_locs[i]
-        avatar_resized = avatar.resize((l, w), IMG.ANTIALIAS)
+        avatar_resized = avatar.resize((l, w), Image.ANTIALIAS)
         frame.paste(avatar_resized, (x, y))
         hand = hand_frames[i]
         frame.paste(hand, mask=hand)
@@ -48,9 +49,9 @@ async def create_petpet(input_path, output_path):
 
 
 async def create_tear(input_path, output_path):
-    avatar = IMG.open(input_path)
-    tear = IMG.open(data_path / 'tear.png')
-    frame = IMG.new('RGBA', (1080, 804), (255, 255, 255, 0))
+    avatar = Image.open(input_path).convert('RGBA')
+    tear = Image.open(data_path / 'tear.png')
+    frame = Image.new('RGBA', (1080, 804), (255, 255, 255, 0))
     left = avatar.resize((385, 385)).rotate(24, expand=True)
     right = avatar.resize((385, 385)).rotate(-11, expand=True)
     frame.paste(left, (-5, 355))
@@ -60,9 +61,43 @@ async def create_tear(input_path, output_path):
     return True
 
 
+async def create_throw(input_path, output_path):
+    avatar = Image.open(input_path).convert('RGBA')
+    throw = Image.open(data_path / 'throw.png')
+    avatar = avatar.resize((143, 143), Image.ANTIALIAS)
+    mask = Image.new('L', avatar.size, 0)
+    draw = ImageDraw.Draw(mask)
+    offset = 1
+    draw.ellipse((offset, offset, avatar.size[0] - offset, avatar.size[1] - offset), fill=255)
+    mask = mask.filter(ImageFilter.GaussianBlur(0))
+    avatar.putalpha(mask)
+    avatar = avatar.rotate(random.randint(1, 360), Image.BICUBIC)
+    throw.paste(avatar, (15, 178), mask=avatar)
+    throw.save(output_path)
+    return True
+
+
+async def create_crawl(input_path, output_path):
+    avatar = Image.open(input_path).convert('RGBA')
+    images = [i for i in (data_path / 'crawl').iterdir() if i.is_file()]
+    crawl = Image.open(random.choice(images)).resize((500, 500), Image.ANTIALIAS)
+    avatar = avatar.resize((100, 100), Image.ANTIALIAS)
+    mask = Image.new('L', avatar.size, 0)
+    draw = ImageDraw.Draw(mask)
+    offset = 1
+    draw.ellipse((offset, offset, avatar.size[0] - offset, avatar.size[1] - offset), fill=255)
+    mask = mask.filter(ImageFilter.GaussianBlur(0))
+    avatar.putalpha(mask)
+    crawl.paste(avatar, (0, 400), mask=avatar)
+    crawl.save(output_path)
+    return True
+
+
 types = {
     'petpet': (create_petpet, '.gif'),
-    'tear': (create_tear, '.png')
+    'tear': (create_tear, '.png'),
+    'throw': (create_throw, '.png'),
+    'crawl': (create_crawl, '.png')
 }
 
 
@@ -75,6 +110,6 @@ async def get_image(user_id: str, type: str):
             if await get_avatar(user_id, avatar_path):
                 if await func(avatar_path, img_path):
                     return str(img_path.absolute())
-    except (AttributeError, TypeError, OSError):
+    except (AttributeError, TypeError, OSError, ValueError):
         logger.debug(traceback.format_exc())
         return ''
