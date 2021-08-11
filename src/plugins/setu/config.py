56b6d15b0eb56b9677ff963569af036d8a4a5d52
meetwq <meetwq@gmail.com>
2021-08-11 16:58:53 +0800

    update lolicon

--- src/plugins/setu/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Config(BaseSettings):
-    setu_apikey: str = ''
-
-    class Config:
-        extra = "ignore"
--- src/plugins/setu/data_source.py
+++ src/plugins/setu/data_source.py
@@ -2,26 +2,22 @@ import aiohttp
 from nonebot import get_driver
 from nonebot.log import logger
 
-from .config import Config
-
-global_config = get_driver().config
-setu_config = Config(**global_config.dict())
-
 
 async def get_pic_url(key_word=None, r18=False) -> str:
-    url = 'https://api.lolicon.app/setu/'
+    url = 'https://api.lolicon.app/setu/v2'
     params = {
-        'apikey': setu_config.setu_apikey,
         'r18': 1 if r18 else 0,
         'num': 1,
-        'size1200': 1,
+        'size': ['regular'],
+        'proxy': 'i.pixiv.cat',
         'keyword': key_word
     }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as resp:
             response = await resp.json()
-    if response['code'] != 0:
+    if response['error']:
+        logger.warning('lolicon error: ' + response['error'])
         return ''
-    result = response['data'][0]['url']
+    result = response['data'][0]['urls']['regular']
     logger.info('Get setu url: ' + result)
     return result
