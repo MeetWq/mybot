9cbf22c51c58c9e00facd4bee928f2d5b2ac694d
meetwq <meetwq@gmail.com>
2021-12-20 18:16:49 +0800

    wordbank

--- src/plugins/wordbank/__init__.py
+++ src/plugins/wordbank/__init__.py
@@ -1,21 +1,105 @@
-from nonebot import on_regex
+import re
+import random
+from nonebot import on_command, on_message
+from nonebot.matcher import Matcher
 from nonebot.typing import T_State
-from nonebot.adapters.cqhttp import Bot, Event
+from nonebot.adapters.cqhttp import Bot, Message, MessageEvent, GroupMessageEvent, unescape
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.cqhttp.permission import GROUP_OWNER, GROUP_ADMIN, PRIVATE_FRIEND
 
-from .data_source import get_response
+from .data_source import WordBank
+from .util import parse
 
 
-nhdgirl = on_regex(r'^[Nn][Hh][Dd]娘', priority=33)
+def get_id(event: MessageEvent):
+    if isinstance(event, GroupMessageEvent):
+        return 'group_' + str(event.group_id)
+    else:
+        return 'private_' + str(event.user_id)
 
 
-@nhdgirl.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    msg = event.get_plaintext().strip().lower().replace('nhd娘', '')
-    if msg:
-        nickname = event.sender.card or event.sender.nickname
-        reply = await get_response(msg, nickname)
-        if reply:
-            nhdgirl.block = True
-            await nhdgirl.finish(reply)
-    nhdgirl.block = False
-    await nhdgirl.finish()
+wb = WordBank()
+
+wordbank = on_message(priority=39)
+
+
+@wordbank.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    user_id = get_id(event)
+    msgs = wb.match(user_id, unescape(event.raw_message))
+    if msgs:
+        msg = random.choice(msgs)
+        await wordbank.finish(Message(unescape(parse(msg, event))))
+
+
+add_cmd = on_command('添加词条', priority=14,
+                     permission=SUPERUSER | GROUP_OWNER | GROUP_ADMIN | PRIVATE_FRIEND)
+add_cmd_gl = on_command('添加全局词条', priority=14, permission=SUPERUSER)
+
+
+@add_cmd.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    await wb_add(add_cmd, event)
+
+
+@add_cmd_gl.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    await wb_add(add_cmd_gl, event, gl=True)
+
+
+async def wb_add(matcher: Matcher, event: MessageEvent, gl: bool = False):
+    user_id = '0' if gl else get_id(event)
+    msg = unescape(event.raw_message)
+    pattern = r"\s*(?:模糊|正则)*\s*问(.+?)答(.+)"
+    match = re.match(pattern, msg, re.S)
+    if match:
+        type, key, value = match.groups()
+        flag = 2 if '模糊' in type else 1 if '正则' in type else 0
+        res = wb.add(user_id, key, value, flag)
+        if res:
+            await add_cmd.finish('我记住了~')
+
+
+rm_cmd = on_command('删除词条', priority=14,
+                    permission=SUPERUSER | GROUP_OWNER | GROUP_ADMIN | PRIVATE_FRIEND)
+rm_cmd_gl = on_command('删除全局词条', priority=14, permission=SUPERUSER)
+
+
+@rm_cmd.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    await wb_rm(rm_cmd, event)
+
+
+@rm_cmd_gl.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    await wb_rm(rm_cmd_gl, event, gl=True)
+
+
+async def wb_rm(matcher: Matcher, event: MessageEvent, gl: bool = False):
+    user_id = '0' if gl else get_id(event)
+    msg = unescape(event.message)
+    res = wb.remove(user_id, msg)
+    if res:
+        await rm_cmd.send('删除成功~')
+
+
+clear_cmd = on_command('清空词库', priority=14,
+                       permission=SUPERUSER | GROUP_OWNER | PRIVATE_FRIEND)
+clear_cmd_gl = on_command('清空全局词库', priority=14, permission=SUPERUSER)
+
+
+@clear_cmd.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    await wb_rm(clear_cmd, event)
+
+
+@clear_cmd_gl.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    await wb_rm(clear_cmd_gl, event, gl=True)
+
+
+async def wb_clear(matcher: Matcher, event: MessageEvent, gl: bool = False):
+    user_id = '0' if gl else get_id(event)
+    res = wb.clear(user_id)
+    if res:
+        await rm_cmd.send('清空成功~')
--- src/plugins/wordbank/data_source.py
+++ src/plugins/wordbank/data_source.py
@@ -1,39 +1,113 @@
 import re
 import json
-import random
 from pathlib import Path
-from nonebot.adapters.cqhttp import Message, MessageSegment
-
-dir_path = Path(__file__).parent
-words_path = dir_path / 'words.json'
+from typing import Optional, List
 
 
-async def get_response(msg, nickname):
-    reply = await get_reply(msg)
-    if reply:
-        reply = format_reply(reply, nickname)
-        return reply
-    return None
+OPTIONS = ['full', 'regex', 'include']
+NULL_BANK = dict((option, {"0": {}}) for option in OPTIONS)
 
 
-async def get_reply(msg):
-    words = json.load(words_path.open('r', encoding='utf-8'))['words']
-    for word in words:
-        for s in word['msg']:
-            if s in msg:
-                return random.choice(word['reply'])
+class WordBank():
+    def __init__(self):
+        dir_path = Path('data/wordbank')
+        self.data_path = dir_path / 'bank.json'
 
-
-def format_reply(reply, nickname):
-    reply = re.sub(r'\[/nickname\]', nickname, reply)
-    p_at = r'(\[/at \d+\])'
-    p_at_qq = r'\[/at (\d+)\]'
-    replies = re.split(p_at, reply)
-    reply = Message()
-    for r in replies:
-        match = re.match(p_at_qq, r)
-        if match:
-            reply.append(MessageSegment.at(match.group(1)))
+        if self.data_path.exists():
+            with self.data_path.open('r', encoding='utf-8') as f:
+                data = json.load(f)
+            self.__data = {key: data.get(key) or {"0": {}}
+                           for key in NULL_BANK.keys()}
         else:
-            reply.append(r)
-    return reply
+            dir_path.mkdir(parents=True, exist_ok=True)
+            self.__data = NULL_BANK
+            self.__save()
+
+    def match(self, user_id: str, msg: str) -> Optional[List]:
+        """
+        匹配词条，匹配顺序：全匹配->正则匹配->模糊匹配
+        :param user_id: 为'0'时是全局词库
+        :param msg: 需要匹配的消息
+        :return: 首先匹配成功的消息列表
+        """
+        for flag in range(len(OPTIONS)):
+            msg = self.__match(user_id, msg, flag)
+            if msg:
+                return msg
+
+    def __match(self, user_id: str, msg: str, flag: int = 0) -> Optional[List]:
+        """
+        匹配词条
+        :param flag: 0: 全匹配（full）（默认）
+                     1: 模糊匹配（include）
+                     2: 正则匹配（regex）
+        """
+        type = OPTIONS[flag]
+        bank = dict(self.__data[type].get(user_id, {}),
+                    **self.__data[type].get("0", {}))
+
+        if flag == 0:
+            return bank.get(msg, [])
+        elif flag == 2:
+            for key in bank:
+                if key in msg:
+                    return bank[key]
+        elif flag == 3:
+            for key in bank:
+                if re.search(key, msg, re.S):
+                    return bank[key]
+
+    def __save(self):
+        with self.data_path.open('w', encoding='utf-8') as f:
+            json.dump(self.__data, f, ensure_ascii=False, indent=4)
+
+    def add(self, user_id: str, key: str, value: str, flag: int = 0) -> bool:
+        """
+        新增词条
+        :param key: 触发短语
+        :param value: 触发后发送的短语
+        """
+        type = OPTIONS[flag]
+
+        if self.__data[type].get(user_id, {}):
+            if self.__data[type][user_id].get(key, []):
+                self.__data[type][user_id][key].append(value)
+            else:
+                self.__data[type][user_id][key] = [value]
+        else:
+            self.__data[type][user_id] = {key: [value]}
+
+        self.__save()
+        return True
+
+    def remove(self, user_id: str, key: str) -> bool:
+        """
+        删除词条
+        """
+        status = False
+        for type in self.__data:
+            if self.__data[type].get(user_id, {}).get(key, False):
+                del self.__data[type][user_id][key]
+                status = True
+        self.__save()
+        return status
+
+    def clear(self, user_id: str) -> bool:
+        """
+        清空某个对象的词库
+        """
+        status = False
+        for type in self.__data:
+            if self.__data[type].get(user_id, {}):
+                del self.__data[type][user_id]
+                status = True
+        self.__save()
+        return status
+
+    def clear_all(self):
+        """
+        清空所有词库
+        """
+        self.__data = NULL_BANK
+        self.__save()
+        return True
