a6420c3ea277bd78992ba34f87f870947e387b37
meetwq <meetwq@gmail.com>
2022-03-19 00:07:16 +0800

    save

--- src/plugins/auto_agree.py
+++ src/plugins/auto_agree.py
@@ -1,3 +1,4 @@
+import asyncio
 from nonebot import on_request
 from nonebot.adapters.onebot.v11 import Bot
 from nonebot.adapters.onebot.v11.event import FriendRequestEvent
@@ -14,6 +15,7 @@ friend_req = on_request(_friend_req, priority=5)
 
 @friend_req.handle()
 async def _(bot: Bot, event: FriendRequestEvent):
+    await asyncio.sleep(5)
     await event.approve(bot)
     await bot.send_private_msg(
         user_id=int(list(bot.config.superusers)[0]),
