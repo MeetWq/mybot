f324e113736d03588b68bee325831c292fa5e6d6
meetwq <meetwq@gmail.com>
2022-03-21 21:25:09 +0800

    nonebot-plugin-simplemusic

--- src/plugins/music/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Extra
-
-
-class Config(BaseModel, extra=Extra.ignore):
-    tencent_secret_id: str = ""
-    tencent_secret_key: str = ""
-    qcloud_region: str = ""
-    qcloud_bucket: str = ""
