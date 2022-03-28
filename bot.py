579a2856d4015fb4f48679dda9645cda9bb0e3b6
meetwq <meetwq@gmail.com>
2022-03-29 03:15:16 +0800

    nonebot-plugin-handle

--- bot.py
+++ bot.py
@@ -52,6 +52,7 @@ nonebot.load_plugin('nonebot_plugin_ddcheck')
 nonebot.load_plugin('nonebot_plugin_simplemusic')
 nonebot.load_plugin('nonebot_plugin_hikarisearch')
 nonebot.load_plugin('nonebot_plugin_wordle')
+nonebot.load_plugin('nonebot_plugin_handle')
 nonebot.load_plugins('src/plugins')
 
 # Modify some config / config depends on loaded configs
