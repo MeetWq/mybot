6c0f899c03830f946340fc12a8cac32427b346aa
meetwq <meetwq@gmail.com>
2024-09-21 20:23:59 +0800

    fix

--- src/plugins/mczju/__init__.py
+++ src/plugins/mczju/__init__.py
@@ -1,11 +1,12 @@
 from nonebot import on_command, on_notice, require
 from nonebot.matcher import Matcher
-from nonebot.rule import to_me
+from nonebot.rule import Rule
 
 require("nonebot_plugin_orm")
 require("nonebot_plugin_saa")
 require("nonebot_plugin_apscheduler")
 
+from nonebot.adapters.onebot.v11 import Bot, Event, PokeNotifyEvent
 from nonebot_plugin_saa import SaaTarget, enable_auto_select_bot
 
 enable_auto_select_bot()
@@ -16,12 +17,16 @@ from .config import dynmap_targets
 from .data_source import get_dynmap_status
 
 
-async def in_dynmap_targets(target: SaaTarget):
+def in_dynmap_targets(target: SaaTarget):
     return target.dict() in [t.dict() for t in dynmap_targets]
 
 
+def poke_rule(bot: Bot, event: Event) -> bool:
+    return isinstance(event, PokeNotifyEvent) and str(event.target_id) == bot.self_id
+
+
 status = on_command("status", rule=in_dynmap_targets, priority=14, block=True)
-poke_status = on_notice(to_me() & in_dynmap_targets, priority=15, block=False)
+poke_status = on_notice(Rule(poke_rule) & in_dynmap_targets, priority=15, block=False)
 
 
 @status.handle()
